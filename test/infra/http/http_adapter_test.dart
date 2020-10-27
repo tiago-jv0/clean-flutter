6f7098c12217329646752cef87f2c2a8bbd738e9
Tiago José <tiago_jvo@outlook.com>
2020-10-27 14:11:34 -0300

    ensure HttpAdapter throws NotFoundError on 404

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -137,6 +137,17 @@ void main() {
       expect(future, throwsA(HttpError.forbidden));
     });
 
+    test('should return NotFoundError if post returns 404', () async {
+      mockResponse(404);
+
+      final future = sut.request(
+        url: url,
+        method: 'post',
+      );
+
+      expect(future, throwsA(HttpError.notFound));
+    });
+
     test('should return ServerError if post returns 500', () async {
       mockResponse(500);
 
