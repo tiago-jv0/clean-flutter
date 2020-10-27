a7bf3ec5f0c0c395b2948454bb12ca63d4a02292
Tiago José <tiago_jvo@outlook.com>
2020-10-27 14:07:43 -0300

    ensure HttpAdapter throws ServerError on 500

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -115,5 +115,16 @@ void main() {
 
       expect(future, throwsA(HttpError.badRequest));
     });
+
+    test('should return ServerError if post returns 500', () async {
+      mockResponse(500);
+
+      final future = sut.request(
+        url: url,
+        method: 'post',
+      );
+
+      expect(future, throwsA(HttpError.serverError));
+    });
   });
 }
