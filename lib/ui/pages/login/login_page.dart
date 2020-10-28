d599f34002a5c6588bcda01c400023ca29ddc9f3
Tiago José <tiago_jvo@outlook.com>
2020-10-28 17:44:00 -0300

    move PasswordInput to its own file

--- lib/ui/pages/login/login_page.dart
+++ lib/ui/pages/login/login_page.dart
@@ -58,26 +58,7 @@ class _LoginPageState extends State<LoginPage> {
                           Padding(
                             padding:
                                 const EdgeInsets.only(top: 8.0, bottom: 32),
-                            child: StreamBuilder<String>(
-                                stream: widget.presenter.passwordErrorStream,
-                                builder: (context, snapshot) {
-                                  return TextFormField(
-                                    onChanged:
-                                        widget.presenter.validatePassword,
-                                    decoration: InputDecoration(
-                                        labelText: 'Senha',
-                                        errorText:
-                                            snapshot.data?.isEmpty == true
-                                                ? null
-                                                : snapshot.data,
-                                        icon: Icon(
-                                          Icons.lock,
-                                          color: Theme.of(context)
-                                              .primaryColorLight,
-                                        )),
-                                    obscureText: true,
-                                  );
-                                }),
+                            child: PasswordInput(),
                           ),
                           StreamBuilder<bool>(
                               stream: widget.presenter.isFormValidStream,
