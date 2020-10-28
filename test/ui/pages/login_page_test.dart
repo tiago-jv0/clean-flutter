1c192b7135855785585dd4b49890938a284b684d
Tiago José <tiago_jvo@outlook.com>
2020-10-28 14:59:57 -0300

    ensure LoginPage enables button if form is valid

--- test/ui/pages/login_page_test.dart
+++ test/ui/pages/login_page_test.dart
@@ -13,14 +13,22 @@ void main() {
   LoginPresenter presenter;
   StreamController<String> emailErrorController;
   StreamController<String> passwordErrorController;
+  StreamController<bool> isFormValidController;
+
   Future<void> loadPage(WidgetTester tester) async {
     presenter = LoginPresenterSpy();
+
     emailErrorController = StreamController<String>();
     passwordErrorController = StreamController<String>();
+    isFormValidController = StreamController<bool>();
+
     when(presenter.emailErrorStream)
         .thenAnswer((realInvocation) => emailErrorController.stream);
     when(presenter.passwordErrorStream)
         .thenAnswer((realInvocation) => passwordErrorController.stream);
+    when(presenter.isFormValidStream)
+        .thenAnswer((realInvocation) => isFormValidController.stream);
+
     final loginPage = MaterialApp(home: LoginPage(presenter));
     await tester.pumpWidget(loginPage);
   }
@@ -28,6 +36,7 @@ void main() {
   tearDown(() {
     emailErrorController.close();
     passwordErrorController.close();
+    isFormValidController.close();
   });
 
   testWidgets('Should load with correct initial state',
@@ -148,5 +157,17 @@ void main() {
     expect(passwordTextChildren, findsOneWidget,
         reason:
             'When a TextFormField has only one text child, means it has no errors, since one of the child is always the label text');
+  });
+
+  testWidgets('Should enable button if form is valid',
+      (WidgetTester tester) async {
+    await loadPage(tester);
+
+    isFormValidController.add(true);
+
+    await tester.pump();
+
+    final button = tester.widget<RaisedButton>(find.byType(RaisedButton));
+    expect(button.onPressed, isNotNull);
   });
 }
