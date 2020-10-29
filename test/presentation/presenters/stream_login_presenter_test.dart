560b85f9e0a3007343c936283f9d738ec6c66ae8
Tiago José <tiago_jvo@outlook.com>
2020-10-29 13:57:46 -0300

    ensure StreamLoginPresenter emits error if email is invalid

--- test/presentation/presenters/stream_login_presenter_test.dart
+++ test/presentation/presenters/stream_login_presenter_test.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:faker/faker.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
@@ -9,29 +11,51 @@ abstract class Validation {
 
 class ValidationSpy extends Mock implements Validation {}
 
+class LoginState {
+  String emailError;
+}
+
 class StreamLoginPresenter {
   final Validation validation;
+
+  final StreamController _controller = StreamController<LoginState>.broadcast();
+
+  LoginState _state = LoginState();
+
+  Stream<String> get emailErrorStream =>
+      _controller.stream.map((state) => state.emailError);
 
   StreamLoginPresenter({@required this.validation});
 
   void validateEmail(String email) {
-    validation.validate(field: 'email', value: email);
+    _state.emailError = validation.validate(field: 'email', value: email);
+    _controller.add(_state);
   }
 }
 
 void main() {
   StreamLoginPresenter sut;
-  ValidationSpy validationSpy;
+  ValidationSpy validation;
   String email;
   setUp(() {
-    validationSpy = ValidationSpy();
-    sut = StreamLoginPresenter(validation: validationSpy);
+    validation = ValidationSpy();
+    sut = StreamLoginPresenter(validation: validation);
 
     final email = faker.internet.email();
   });
   test('should call validation with correct email', () {
     sut.validateEmail(email);
 
-    verify(validationSpy.validate(field: 'email', value: email)).called(1);
+    verify(validation.validate(field: 'email', value: email)).called(1);
+  });
+
+  test('should emit validation error if email fails', () {
+    when(validation.validate(
+            field: anyNamed('field'), value: anyNamed('value')))
+        .thenReturn('error');
+
+    expectLater(sut.emailErrorStream, emits('error'));
+
+    sut.validateEmail(email);
   });
 }
