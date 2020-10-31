f57ca26b2fee39663c728eaa1e232299119892fa
Tiago José <tiago_jvo@outlook.com>
2020-10-31 01:57:46 -0300

    ensure StreamLoginPresenter emits password error as null if validation succeeds

--- test/presentation/presenters/stream_login_presenter_test.dart
+++ test/presentation/presenters/stream_login_presenter_test.dart
@@ -78,4 +78,15 @@ void main() {
     sut.validatePassword(password);
     sut.validatePassword(password);
   });
+
+  test('should emit null if validation succeeds', () {
+    sut.passwordErrorStream
+        .listen(expectAsync1((error) => expect(error, null)));
+
+    sut.isFormValidStream
+        .listen(expectAsync1((isValid) => expect(isValid, false)));
+
+    sut.validatePassword(password);
+    sut.validatePassword(password);
+  });
 }
