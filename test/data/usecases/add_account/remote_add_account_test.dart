60fe6e7f730fd8665ea2f2370c382bc66dc306c2
Tiago José <tiago_jvo@outlook.com>
2020-11-12 03:33:36 -0300

    ensure RemoteAddAccount throws UnexpectedError on 200 with invalid data

--- test/data/usecases/add_account/remote_add_account_test.dart
+++ test/data/usecases/add_account/remote_add_account_test.dart
@@ -93,4 +93,14 @@ void main() {
 
     expect(account.token, validData['accessToken']);
   });
+
+  test(
+      'should throw unexpected error if HttpClient returns 200 with invalid data ',
+      () async {
+    mockHttpData({'invalid_key': 'invalid_value'});
+
+    final future = sut.add(params);
+
+    expect(future, throwsA(DomainError.unexpected));
+  });
 }
