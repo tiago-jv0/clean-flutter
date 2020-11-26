79e7a0f742333c5bf0593316892897c8744d00b7
Tiago José <tiago_jvo@outlook.com>
2020-11-26 14:53:27 -0300

    ensure CompareFieldsValidation returns InvalidFieldError if values are not equal

--- test/validation/validators/compare_fields_validation_test.dart
+++ test/validation/validators/compare_fields_validation_test.dart
@@ -1,5 +1,6 @@
 import 'package:ForDev/validation/validators/validators.dart';
 import 'package:test/test.dart';
+import 'package:ForDev/presentation/protocols/protocols.dart';
 
 void main() {
   CompareFieldsValidation sut;
@@ -7,6 +8,10 @@ void main() {
   setUp(() {
     sut = CompareFieldsValidation(
         field: 'any_field', valueToCompare: 'any_value');
+  });
+  test('Should return InvalidFieldError if values are not equal', () {
+    final error = sut.validate('');
+    expect(error, ValidationError.invalidField);
   });
 
   test('Should return null if values are equal', () {
