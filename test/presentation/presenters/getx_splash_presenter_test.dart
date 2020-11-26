93cc5cce2d08bd1d7fd35dd2b43459ede949b389
Tiago José <tiago_jvo@outlook.com>
2020-11-26 17:09:01 -0300

    ensure SplashPresenter goes to login if token is null

--- test/presentation/presenters/getx_splash_presenter_test.dart
+++ test/presentation/presenters/getx_splash_presenter_test.dart
@@ -50,6 +50,14 @@ void main() {
     await sut.checkAccount(durationInSeconds: 0);
   });
 
+  test('Should go to login page on null token', () async {
+    mockLoadCurrentAccount(account: AccountEntity(null));
+
+    sut.navigateToStream.listen(expectAsync1((page) => expect(page, '/login')));
+
+    await sut.checkAccount(durationInSeconds: 0);
+  });
+
   test('Should go to login page on error', () async {
     mockLoadCurrentAccountError();
 
