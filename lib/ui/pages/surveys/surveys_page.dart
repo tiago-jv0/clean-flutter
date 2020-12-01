70044033c370631d64b0a11fe972aba68a89601b
Tiago José <tiago_jvo@outlook.com>
2020-12-01 15:59:05 -0300

    ensure SurveysPage handles loading correctly

--- lib/ui/pages/surveys/surveys_page.dart
+++ lib/ui/pages/surveys/surveys_page.dart
@@ -1,9 +1,10 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
-import './surveys_presenter.dart';
+import 'surveys_presenter.dart';
 
 import '../../helpers/helpers.dart';
+import '../../components/components.dart';
 
 class SurveysPage extends StatelessWidget {
   final SurveysPresenter presenter;
@@ -18,15 +19,27 @@ class SurveysPage extends StatelessWidget {
         title: Text(R.strings.surveys),
         centerTitle: true,
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 20.0),
-        child: CarouselSlider(
-          items: [],
-          options: CarouselOptions(
-            enlargeCenterPage: true,
-            aspectRatio: 1,
-          ),
-        ),
+      body: Builder(
+        builder: (context) {
+          presenter.isLoadingStream.listen((isLoading) {
+            if (isLoading == true) {
+              showLoading(context);
+            } else {
+              hideLoading(context);
+            }
+          });
+
+          return Padding(
+            padding: const EdgeInsets.symmetric(vertical: 20.0),
+            child: CarouselSlider(
+              items: [],
+              options: CarouselOptions(
+                enlargeCenterPage: true,
+                aspectRatio: 1,
+              ),
+            ),
+          );
+        },
       ),
     );
   }
--- test/ui/pages/surveys_page_test.dart
+++ test/ui/pages/surveys_page_test.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:ForDev/ui/pages/pages.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:get/get.dart';
 import 'package:mockito/mockito.dart';
@@ -10,8 +11,25 @@ class SurveysPresenterSpy extends Mock implements SurveysPresenter {}
 void main() {
   SurveysPresenterSpy presenter;
 
+  StreamController<bool> isLoadingController;
+
+  void initStreams() {
+    isLoadingController = StreamController<bool>();
+  }
+
+  void mockStreams() {
+    when(presenter.isLoadingStream)
+        .thenAnswer((realInvocation) => isLoadingController.stream);
+  }
+
+  void closeStreams() {
+    isLoadingController.close();
+  }
+
   Future<void> loadPage(WidgetTester tester) async {
     presenter = SurveysPresenterSpy();
+    initStreams();
+    mockStreams();
     final surveysPage = GetMaterialApp(
       initialRoute: '/surveys',
       getPages: [
@@ -26,10 +44,30 @@ void main() {
     await tester.pumpWidget(surveysPage);
   }
 
+  tearDown(() {
+    closeStreams();
+  });
+
   testWidgets('Should call LoadSurveys on page load',
       (WidgetTester tester) async {
     await loadPage(tester);
 
     verify(presenter.loadData()).called(1);
+  });
+
+  testWidgets('Should handle loading correctly', (WidgetTester tester) async {
+    await loadPage(tester);
+
+    isLoadingController.add(true);
+    await tester.pump();
+    expect(find.byType(CircularProgressIndicator), findsOneWidget);
+
+    isLoadingController.add(false);
+    await tester.pump();
+    expect(find.byType(CircularProgressIndicator), findsNothing);
+
+    isLoadingController.add(null);
+    await tester.pump();
+    expect(find.byType(CircularProgressIndicator), findsNothing);
   });
 }
