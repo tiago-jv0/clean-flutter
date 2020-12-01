7eef0dc08d6d90856e72c181d21baa3c6457d121
Tiago José <tiago_jvo@outlook.com>
2020-12-01 12:14:11 -0300

    ensure HttpAdapter returns data on 200

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -210,5 +210,25 @@ void main() {
         },
       )).called(1);
     });
+
+    test('should return data if get returns 200', () async {
+      final response = await sut.request(
+        url: url,
+        method: 'get',
+      );
+
+      expect(response, {'any_key': 'any_value'});
+    });
+
+    test('should return null if get returns 200 with no data', () async {
+      mockResponse(200, body: '');
+
+      final response = await sut.request(
+        url: url,
+        method: 'get',
+      );
+
+      expect(response, null);
+    });
   });
 }
